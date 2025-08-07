552a4faab58ccae513b0c7871d33d9e0578971ce
Quelsec <60035185+Quelsec@users.noreply.github.com>
2025-08-07 19:09:28 +0200

    Update siodmy.js

--- js/siodmy.js
+++ js/siodmy.js
@@ -8,11 +8,14 @@ function kwadrat(a,b,c) {
         if (delta > 0) {
             let x1=(-b - Math.sqrt(delta)) /(2*a);
             let x2= (-b + Math.sqrt(delta)) /(2*a); 
+            console.log("2 rozwiązania");
         } else if (delta===0) {
             let x= -b/(2*a);
+            console.log("1 rozwiazanie", x)
         }
         else {
             console.log("brak rozwiązań rzeczywistych");
         }
     }
 }
+kwadrat(10,10,1)
